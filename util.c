3744024eb0009c3326d8ba62e49d33a657d85e40
Karl Williamson <khw@cpan.org>
2021-03-13 15:30:32 -0700

    util.c: mktime needs to run under a mutex
    per the Posix standard

--- util.c
+++ util.c
@@ -4202,7 +4202,9 @@ giving localized results.
   STMT_START {
     struct tm mytm2;
     mytm2 = mytm;
+    MKTIME_LOCK;
     mktime(&mytm2);
+    MKTIME_UNLOCK;
 #ifdef HAS_TM_TM_GMTOFF
     mytm.tm_gmtoff = mytm2.tm_gmtoff;
 #endif
