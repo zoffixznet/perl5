2082bc212c265f0b1613c1ac75d07b629ace0e67
Karl Williamson <khw@cpan.org>
2021-04-09 12:18:22 -0600

    sv.c: Comments white-space only

--- sv.c
+++ sv.c
@@ -4566,11 +4566,11 @@ Perl_sv_setsv_flags(pTHX_ SV *dsv, SV* ssv, const I32 flags)
 	 * be allocated it is still not worth swiping PADTMPs for short
 	 * strings, as the savings here are small.
 	 *
-	 * If swiping is not an option, then we see whether it is
-	 * worth using copy-on-write.  If the lhs already has a buf-
-	 * fer big enough and the string is short, we skip it and fall back
-	 * to method 3, since memcpy is faster for short strings than the
-	 * later bookkeeping overhead that copy-on-write entails.
+         * If swiping is not an option, then we see whether it is worth using
+         * copy-on-write.  If the lhs already has a buffer big enough and the
+         * string is short, we skip it and fall back to method 3, since memcpy
+         * is faster for short strings than the later bookkeeping overhead that
+         * copy-on-write entails.
 
 	 * If the rhs is not a copy-on-write string yet, then we also
 	 * consider whether the buffer is too large relative to the string
@@ -15663,8 +15663,7 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
 #ifdef USE_LOCALE_COLLATE
     PL_collation_name	= SAVEPV(proto_perl->Icollation_name);
     PL_in_utf8_COLLATE_locale = proto_perl->Iin_utf8_COLLATE_locale;
-#endif /* USE_LOCALE_COLLATE */
-
+#endif
 #ifdef USE_LOCALE_NUMERIC
     PL_numeric_name	= SAVEPV(proto_perl->Inumeric_name);
     PL_numeric_radix_sv	= sv_dup_inc(proto_perl->Inumeric_radix_sv, param);
